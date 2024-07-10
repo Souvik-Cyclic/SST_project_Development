8c367dd64a3196bf4fae809d57d687a66b86dfcd
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-10 11:08:37 +0530

    implemented password hashing

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 router.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({email:req.body.email})
+    if(userExists){
+        return res.json({success: false, message: "User Already Exists"});
+    }
 
+    const salt =  await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
 
+    const newUser = new User(req.body);
+    await newUser.save();
+    return res.status(201).json("User Created Successfully");
+  } catch (error) {
+    return res.json(error);
+  }
 });
 
-router.post("/login", async (req, res) => {
-  
-});
-
+router.post("/login", async (req, res) => {});
 
 module.exports = router;
