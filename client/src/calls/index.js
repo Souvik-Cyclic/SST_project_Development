55813f5a8d87313eee9bfc4a9ccb4722e1737b1c
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-12 23:06:14 +0530

    implemented login through jwt stored in local storage

--- client/src/calls/index.js
+++ client/src/calls/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('token');
+
 export const axiosInstance = axios.create({
     headers: {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json',
+        'authorization': `Bearer ${token}`,
     }
-})
+});
